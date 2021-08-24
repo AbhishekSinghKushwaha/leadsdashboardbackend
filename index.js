bfb32288c272d070b68fb3f7e611fbca53ed0735
AbhishekSinghKushwaha <abhisheksinghs.1031@gmail.com>
2021-08-24 17:01:27 +0530

    added procfile

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(cors({origin:'http://localhost:3000'}));
 
+app.get('/', (req, res) => {
+  res.send('Leads API')
+})
 app.use('/leadsdashboard',leadsController);
 app.use('/user',userController);
 app.use('/todo', todoController);
